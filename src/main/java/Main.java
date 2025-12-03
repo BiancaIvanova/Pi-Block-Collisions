6c974f9f4c3f489e7fcae76895af861783301e44
Bianca Ivanova <bianca.ivanova.1.0@gmail.com>
2025-12-03 18:25:56 +0000

    Fixed directory structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,6 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Scanner;
 
-// m1u1 + m2u2 = m1v1 + m2v2
-// (v2 - v1) / (u1 - u2)
-// v2 = v1 + (u2 - u1)
-// m1u1 + m2u2 = m1v1 + m2(v1 + (u2 - u1))
-// u1 = (m2v1 + m1v2) / (m1 + m2)
-
-// u1 + m2u2 = v1 + m2v2
-// v1 = u1 + m2u2 - m2v2    ONE
-
-// v2 - v1 = u1 - u2
-// v2 = u1 - u2 + v1    TWO
-// v2 = u1 - u2 + u1 + m2u2 - m2v2
-// v2 + m2v2 = u1 - u2 + u1 + m2u2
-// v2(m2 + 1) = u1 - u2 + u1 + m2u2
-// v2 = (u1 - u2 + u1 + m2u2) / (m2 + 1)
-// v2 = (2u1 - u2 + m2u2) / (m2 + 1)
-
-// v2 = (2u1 - u2 + m2u2) / (m2 + 1)    Equation for v2
-
 public class Main
 {
     void main()
